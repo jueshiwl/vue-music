a5b0274e7e375729f258e831aeca53975fea90fd
jueshiwl <469078133@qq.com>
2017-07-11 20:48:12 +0800

    'change'

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <m-header></m-header>
+        <tab></tab>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import mHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+
+export default {
+    components: {
+        mHeader,
+        Tab
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "common/sass/index.scss";
+    @import "common/sass/variable.scss";
+    #app {
+        color: $color-theme;
+    }
+</style>
